7ad21dc1efb8978cb5bfe04cd6c66fe01f053841
Jvbass <juan.vpinoc@gmail.com>
2024-03-26 23:57:01 -0300

    Se agrega condicional if en clase Transacciones en su metodo listarMovimientos para diferenciar tipoMovimiento y agregar simbolo - en retiro

---
+++ src/main/java/cl/jpinoc/virtualwallet/service/Transacciones.java
@@ -0,0 +1,45 @@
+package cl.jpinoc.virtualwallet.service;
+
+import cl.jpinoc.virtualwallet.controller.interfaces.ITransaccion;
+import cl.jpinoc.virtualwallet.model.entity.Movimientos;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Transacciones implements ITransaccion{
+
+    private List<Movimientos> movimiento;
+
+    public Transacciones() {
+        this.movimiento = new ArrayList<>();
+    }
+
+    public List<Movimientos> getMovimiento() {
+        return movimiento;
+    }
+
+    public void setMovimiento(List<Movimientos> movimiento) {
+        this.movimiento = movimiento;
+    }
+
+    public void agregarMovimiento(Movimientos movimiento){
+        this.movimiento.add(movimiento);
+    }
+
+
+    public void listarMovimientos(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        if (movimiento.isEmpty()) {
+            System.out.println("No hay movimientos que mostrar");
+        } else {
+            for (Movimientos movimiento : movimiento) {
+                if (movimiento.getTipoMovimiento().equals("Deposito")) {
+                    System.out.println("Deposito: " + movimiento.getMonto() + " " + movimiento.getFecha() + " " + movimiento.getHora().format(formatter) + "\n");
+                } else {
+                    System.out.println("Retiro: -" + movimiento.getMonto() + " " + movimiento.getFecha() + " " + movimiento.getHora().format(formatter) + "\n");
+                }
+            }
+        }
+    }
+}
